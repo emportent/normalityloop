55e6928d6f5c1e03074bc633170aef4a5276e5e7
Enes <54986652+emportent@users.noreply.github.com>
2021-02-08 16:53:58 +0300

    Update normalty.R

--- normalty.R
+++ normalty.R
@@ -5,28 +5,14 @@ i=1
 while(TRUE){
 for(i in df[]){#replace df[] with your data for instance df[24:100]
   
-  
-  
   a=shapiro.test(i)
-  
   df2<-df[] #your df 
-
-
   print(a)
-
-
 }
-
-
 a=i+1
-
-  
-for(names in colnames(df[]){  #your df 
-  
-  
+for(names in colnames(df[])){  #your df 
   df2%>% pull(names)%>% hist(,xlab = names[],col="orange")
   i=i+1
-
 }
    return(i)
 }
